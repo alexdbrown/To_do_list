4dcc52d9c8fabc5356489d9c1edd210058c46eb1
Deron Johnson & alexandra brown <student@epicodus.com>
2015-08-17 14:34:58 -0700

    Add routes to app.php

--- app/app.php
+++ app/app.php
@@ -5,6 +5,8 @@
 
     $app = new Silex\Application();
 
+    $app['debug'] = true;
+
     $server = 'mysql:host=localhost;dbname=to_do';
     $username = 'root';
     $password = 'root';
@@ -26,17 +28,28 @@
     });
 
     $app->get("/categories", function() use ($app) {
+        return $app["twig"]->render('categories.html.twig', array('categories' => Category::getAll()));
+    });
+
+    $app->post("/categories", function() use ($app) {
+        $category = new Category($_POST['name']);
+        $category->save();
         return $app['twig']->render('categories.html.twig', array('categories' => Category::getAll()));
     });
 
     $app->post("/tasks", function() use ($app) {
         $task = new Task($_POST['description']);
-        $task-save();
+        $task->save();
         return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getAll()));
     });
 
     $app->post("/delete_tasks", function() use ($app) {
         Task::deleteAll();
+        return $app['twig']->render('index.html.twig');
+    });
+
+    $app->post("/delete_categories", function() use ($app) {
+        Category::deleteAll();
         return $app['twig']->render('index.html.twig');
     });
 
